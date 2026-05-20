d07b90d127fbd945903f4b152fd25bb32d7e8381
Charles <dae.charles.kuo@gmail.com>
2026-05-20 16:35:10 +0800

    release(staging) 1.0.4 (8)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.dae.stemscampus"
         minSdk = 29
         targetSdk = 36
-        versionCode = 7
-        versionName = "1.0.3"
+        versionCode = 8
+        versionName = "1.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
